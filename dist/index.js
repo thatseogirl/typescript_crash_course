54ec1cef973282bb92209370c750152b3581d57f
Tobiloba <sm@MacBook-Pro.local>
2025-03-06 12:29:48 +0100

    add more types

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,45 @@
 "use strict";
+//basic types
 let id = 5;
-console.log("ID:", id);
+let age = 8;
+let maritalStatus = true;
+let company = "mypersonal";
+let x = 7;
+//array types
+let arrays = [{ "key": "value" }];
+let anyArray = [];
+//Tuple arrays
+let tupleArray1 = [1, "shshsh", true]; //single array
+let tupleArray2 = [
+    [1, "sample"],
+    [2, "samle2"]
+];
+//unions
+let pid = "samson";
+//enum
+var Directions1;
+(function (Directions1) {
+    Directions1[Directions1["up"] = 0] = "up";
+    Directions1[Directions1["down"] = 1] = "down";
+    Directions1[Directions1["left"] = 2] = "left";
+    Directions1[Directions1["right"] = 3] = "right";
+})(Directions1 || (Directions1 = {}));
+const user = {
+    id: 1,
+    name: "shade"
+};
+//type Assertion
+let cid = 1;
+let typeAssertionTest = cid;
+// or 
+let typeAssertionTest2 = cid;
+typeAssertionTest = "8";
+//function types
+function addNumber(x, y) {
+    return x + y;
+}
+//functions with no returns 
+function noReturn(message) {
+    console.log(message);
+}
+noReturn("you're welcome here ");
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,54 @@
+//basic types
 let id: number = 5
+let age: number = 8
+let maritalStatus: boolean = true
+let company: string = "mypersonal"
+let x: any = 7
+//array types
+let arrays: object[]= [{"key": "value"}]
+let anyArray: any[]= []
 
-console.log("ID:", id)
+//Tuple arrays
+let tupleArray1: [number,string,boolean] = [1, "shshsh", true] //single array
+let tupleArray2: [number,string][] = [                         //multiple array
+    [1, "sample"],
+    [2, "samle2"]
+]
+
+//unions
+let pid: string | number = "samson"
+
+//enum
+enum Directions1  {
+    up,down,left,right
+}
+
+//object tyoe
+type User = {
+    id: number,
+    name: string
+}
+
+const user: User = {
+    id: 1,
+    name: "shade"
+}
+
+//type Assertion
+let cid: any = 1
+let typeAssertionTest= <string>cid   
+// or 
+let typeAssertionTest2= cid as string
+
+typeAssertionTest = "8"
+
+//function types
+function addNumber(x: number,y: number): number {
+ return x + y
+}
+
+//functions with no returns 
+function noReturn (message: string | number): void{
+    console.log(message)
+}
+noReturn("you're welcome here ")
